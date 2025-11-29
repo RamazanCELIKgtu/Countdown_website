487f1bd947c617cebd6ec855dac01c58428f9736
RamazanCELIKgtu <ramazanbullet@gmail.com>
2025-11-29 23:42:58 +0100

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Countdown_website/', // Replace with your exact repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
